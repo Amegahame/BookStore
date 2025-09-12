a9ed623169d020dae4623a05329adc279a1c75fc
diogo patrick <diogo_p@hotmail.com.br>
2025-09-11 22:49:46 -0300

    Implementar ViewSets e testes unitários para Product, Order e Category

---
+++ product/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework import routers
+
+
+from product import viewsets
+from product.viewsets import CategoryViewSet
+
+router = routers.SimpleRouter()
+router.register(r'product', viewsets.ProductViewSet, basename='product')
+router.register(r'categories', CategoryViewSet, basename='category')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
